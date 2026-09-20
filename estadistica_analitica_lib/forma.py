ee7731c42c7f37c4063ed0593a02dd2372679438
Moisés Rojas <rojasmata.moy@gmail.com>
2026-09-20 13:07:05 -0600

    Modulo forma

--- estadistica_analitica_lib/forma.py
+++ estadistica_analitica_lib/forma.py
@@ -36,7 +36,34 @@ def coef_fp_momentos(datos_frecuencias, datos_tendencias):
 def coef_asim_p1(datos_tendencias, datos_dispersion):
     media = datos_tendencias["Media Aritmetica"]
     moda = datos_tendencias["Moda"]
-    varianza = datos_dispersion["Varianza"]
+    sigma = datos_dispersion["Desviacion Estandar"]
 
-    return (media - moda) / varianza
+    return (media - moda) / sigma
 
+def coef_asim_p2(datos_tendencias, datos_dispersion):
+    media = datos_tendencias["Media Aritmetica"]
+    mediana = datos_tendencias["Mediana"]
+    sigma = datos_dispersion["Desviacion Estandar"]
+    
+    return (3 * (media - mediana)) / sigma
+
+def coef_sesgo_cuantiles(datos_tendencias):
+    q1 = datos_tendencias["Cuartil 1"]
+    q2 = datos_tendencias["Cuartil 2"]
+    q3 = datos_tendencias["Cuartil 3"]
+    
+    return (q3 + q1 - 2 * q2) / (q3 - q1)
+
+def exceso_curtosis(datos_frecuencias, datos_tendencias):
+    m4 = momento_central(4, datos_frecuencias, datos_tendencias)
+    m2 = momento_central(2, datos_frecuencias, datos_tendencias)
+    
+    return (m4 / (m2 ** 2)) - 3
+
+def curtosis_percentilica(datos_tendencias):
+    p90 = datos_tendencias["Percentil 90"]
+    p75 = datos_tendencias["Percentil 75"]
+    p25 = datos_tendencias["Percentil 25"]
+    p10 = datos_tendencias["Percentil 10"]
+    
+    return (p90 - p10) / (p75 - p25)
